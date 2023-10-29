45805c19b87ec0e594e1269c9a69ac56dbdd4f22
Akhil Kumar MNS <68002153+AK2207@users.noreply.github.com>
2023-10-29 17:08:26 +0530

    reverse_string.c

--- reverse_string.c
+++ reverse_string.c
@@ -21,3 +21,15 @@ printf("reversed string: %s\n", str);
 
     return 0;
 }
+
+/*
+cnt = sizeof(str)/sizeof(str[0]);
+printf("no of char in string: %d\n", cnt);
+
+for (int i=0 ; i<=((cnt)/2)-1; i++){
+    temp = str[i];
+    str[i]= str[cnt-2-i];
+    str[cnt-2-i] = temp;
+    
+}
+*/
